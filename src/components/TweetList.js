237537f7d8906527a31f7ab125d8c42967fffbf8
Josh Cavin <jcavin9@gmail.com>
2019-12-29 10:59:24 -0800

    Added tweetlist

---
+++ src/components/TweetList.js
@@ -0,0 +1,47 @@
+import React, { useContext, useState } from 'react'
+import Store from "../context";
+
+export default function TweetList() {
+  const [tweetBody, setTweetBody] = useState({ userId: '', body: '' })
+  const { state, dispatch } = useContext(Store);
+
+  function handleUpdate(user, tweet) {
+    return () => dispatch(
+      {
+        type: "UPDATE_TWEET",
+        payload: {
+          userId: user.id,
+          tweetId: tweet.id,
+          body: document.getElementById(tweet.id).value
+        }
+      }
+    )
+  }
+
+  return(
+    <ul>
+      {
+        state.users.map(
+          user => (
+            <li key={user.id}>
+              {user.name}
+              <ul>
+                {
+                  user.tweets.map(
+                    tweet => (
+                      <li key={tweet.id}>
+                        {tweet.body}
+                        <input id={tweet.id} />
+                        <button onClick={handleUpdate(user, tweet)}>update</button>
+                      </li>
+                    )
+                  )
+                }
+              </ul>
+            </li>
+          )
+        )
+      }
+    </ul>
+  )
+}
